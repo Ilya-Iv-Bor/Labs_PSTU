9f7c5221099482d2f8d333a49e1e3c818b8d85c7
Ilya-Iv-Bor <63847892+Ilya-Iv-Bor@users.noreply.github.com>
2020-05-22 20:25:33 +0500

    Add files via upload

---
+++ 16 (9э)Методы поиска Бойера-мура и интерполяции/Бойера-мура.cpp
@@ -0,0 +1,45 @@
+# include <limits.h>
+# include <string.h>
+# include <stdio.h>
+#include <iostream>
+using namespace std;
+
+# define NO_OF_CHARS 256
+
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+void badCharHeuristic(char* str, int size, int badchar[NO_OF_CHARS]) {
+	int i;
+	for (i = 0; i < NO_OF_CHARS; i++) badchar[i] = -1;
+	for (i = 0; i < size; i++) badchar[(int)str[i]] = i;
+}
+
+void search(char* txt, char* pat) {
+	int m = strlen(pat);
+	int n = strlen(txt);
+	int badchar[NO_OF_CHARS];
+	badCharHeuristic(pat, m, badchar);
+	int s = 0; 
+	while (s <= (n - m)) {
+		int j = m - 1;
+
+		while (j >= 0 && pat[j] == txt[s + j]) j--;
+
+		if (j < 0) {
+			printf("\nThe search part begins after the position: %d", s);
+			s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+		} else s += max(1, j - badchar[txt[s + j]]);
+	}
+}
+
+int main() {
+	cout << "Source string: " << endl;
+	char txt[] = "We love computer science";
+	cout << txt << endl <<"The desired part:";
+	char pat[] = "comp";
+	cout << pat;
+	search(txt, pat);
+	return 0;
+}
